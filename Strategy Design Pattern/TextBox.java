e5c9eedda25a520433166efe3ba7cbc2c3132a4c
Bhasker-Gopal <84311681+Bhasker-Gopal@users.noreply.github.com>
2021-05-20 11:58:09 +0530

    Create TextBox.java

---
+++ Strategy Design Pattern/TextBox.java
@@ -0,0 +1,20 @@
+package com.strategy; 
+
+public class TextBox implements UIElementalControlWidgit 
+{ 
+	private double height;
+	private double width;
+	
+	public Button (double height, double width)
+	{
+		this.height = height;
+		this.width = width;
+	}
+
+	@override
+	public void control()
+	{
+		//code
+	}
+	 
+}
