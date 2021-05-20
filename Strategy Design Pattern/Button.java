07dd7a1ab4812119b1e31628cd6e8919e80d8ad1
Bhasker-Gopal <84311681+Bhasker-Gopal@users.noreply.github.com>
2021-05-20 11:57:21 +0530

    Create Button.java

---
+++ Strategy Design Pattern/Button.java
@@ -0,0 +1,20 @@
+package com.strategy; 
+
+public class Button implements UIElementalControlWidgit 
+{ 
+	private double height;
+	private double width;
+	
+	public Button (double height, double width)
+	{
+		this.height = height;
+		this.width = width;
+	}
+
+	@override
+	public void control()
+	{
+		//code
+	}
+	 
+}
